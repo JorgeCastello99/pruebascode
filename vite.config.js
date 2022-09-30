b66bb5a5c151cad2bbcffbc24527b0af13eb8665
Jorge Castelló <jcaste999@gmail.com>
2022-09-30 18:06:36 +0200

    pruebas

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import path from "path";
+const isGitHubPages = true;
+const folderName = "./";
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const base = mode === "production" && isGitHubPages ? "" + folderName : "/";
+
+export default {
+  root: "src",
+  base,
+  mode,
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    assetsDir: "./"
+  }
+};
